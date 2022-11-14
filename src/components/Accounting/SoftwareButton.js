ea451c13030ad9c77183572eff432a1e5f5222c4
Jaydeep Patel <58411819+jaydeep-patel-au6@users.noreply.github.com>
2022-11-14 19:00:42 +0530

    start to implement quickbooks flow

--- src/components/Accounting/SoftwareButton.js
+++ src/components/Accounting/SoftwareButton.js
@@ -1,14 +1,44 @@
 import React from "react";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { buttonData } from "../../data/data";
+import { authUrl, sandboxUrl, productionSandBox } from "../../data/const";
 
 const SoftwareButton = () => {
   const navigate = useNavigate();
+
+  // const config = {
+  //   headers: {
+  //     Accept: "application/json",
+  //     Authorization:
+  //       "Bearer eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwiYWxnIjoiZGlyIn0..3iWb1u3YnyVYFw4Tlaqcfg.atjV4Z3YR2RSO5k1Ure7v1kLA2MmoZQO1jOrUbmI4pTkuSox6fFqw82R_Kc-YR8GYfUaZ5C4jQjsogOUzC98vGaNWtg8WIQQo9Ogqs4cxMZ-iFi6B8b_KAKo28DFpGieG2NrM58P2zjAWLucc2P9X2QJA7X9Tb_06cTuRLCcT83WHmaSWZ85XsVJfe8y1NGnqZW4WgN6R9WGORWwDs35Ih8wq9IZALDIv-Zle-QPkNomZL4lICH_7vee7v-LAbEMIeaU4PvcE3mxI8qW9_ZVi4nitKF5gblADVnbzOBsJastKgqP1nKqgh1CWOQoS2WdURBeTnjlhpPbTbTyqbcIjEN15YBecx9fRNug7hrkN6eqIIyqqIxQ3oqR1dnnJYutimaSgUKBeTaZxcxH2CL75nln6f55UIe766s_YayfXhuwXrYLxIhvT2ZNM6z5NaZBPjyFPq8Nq9GL0t7CAtnhCzVAloiExn6AUdBCEylDbBZi1694u6W54PW-CHp2h4GYbY5XnoDZQZRoEU5Qr_7U3ZuWC6Q7vLALy3fiNxoLaKRdYvRU7gkY6UARfRHCE_DX4yIfTHohybqnpEyo-ZwLGrVJ5blD7yUK4wt_Nv8k5jv9j0Bz9-xGiwi-9Sm42_YVrvrOjYstq7nPbXwheH5kOwC84bkKsosYJJBxFT4AQB_M8XVVJpyTzqGScDNvQaidAHTPXCzBfT4XUS51uv1LvFxG81EtRnmuYKkFyMFBIdDPIrLVnC4yCbKC0mgZW2LT._4jP9PiswbEaES6NNdDXVg",
+  //   },
+  // };
+
+  // const getToken = () => {
+  //   axios
+  //     .get(
+  //       "https://backend-codat.herokuapp.com/api/fetchQuickBookInvoice/4620816365253336770",
+  //       config
+  //     )
+  //     .then((res) => console.log(res))
+  //     .catch((err) => console.log(err));
+  // };
+
   const selectAuth = (data) => {
-    if (data?.name === "sandbox" || data?.name === "quickbooksonline") {
+    // getToken();
+    if (data?.name === "sandbox") {
       navigate("/add-company", { state: data?.name });
     }
+    if (data?.name === "quickbooksonline") {
+      window.open(
+        authUrl,
+        "",
+        "toolbar=no,status=no,menubar=no,location=center,scrollbars=no,resizable=no,height=500,width=657"
+      );
+    }
   };
+
   return (
     <div>
       <div className="mt-10 text-lg font-bold">
