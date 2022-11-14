339b666b46235ce05f022465deae6a95d8b55b12
Jaydeep Patel <58411819+jaydeep-patel-au6@users.noreply.github.com>
2022-11-14 18:59:56 +0530

    add urls

--- src/data/const.js
+++ src/data/const.js
@@ -1,1 +1,5 @@
 export const baseURL = "https://backend-codat.herokuapp.com/";
+export const authUrl =
+  "https://accounts.intuit.com/index.html?redirect_url=https%3A%2F%2Fappcenter.intuit.com%2Fapp%2Fconnect%2Foauth2%3Fclient_id%3DABdEMEJ1jI3Hn1dqmYKI4kKBw5RphcaaDajtzojRJ67fSnsBq9%26scope%3Dcom.intuit.quickbooks.accounting%2520openid%2520profile%2520email%2520phone%2520address%26redirect_uri%3Dhttps%253A%252F%252Fdeveloper.intuit.com%252Fv2%252FOAuth2Playground%252FRedirectUrl%26response_type%3Dcode%26state%3DCFNUE&offering_id=Intuit.sbg-fms.ippdevx&partner_uid_button=google&appfabric=true";
+// export const productionSandBox = "https://quickbooks.api.intuit.com";
+// export const sandboxUrl = "https://sandbox-quickbooks.api.intuit.com";
