51823f77ef544ea485865f10b9cf1fddcb957a53
Jaydeep Patel <58411819+jaydeep-patel-au6@users.noreply.github.com>
2022-11-29 17:53:57 +0530

    change spell

--- src/components/AddCompany/AddCompanyName.js
+++ src/components/AddCompany/AddCompanyName.js
@@ -6,7 +6,7 @@ const AddCompanyName = ({ platformName }) => {
     <div>
       <div className="w-[100vw] xl:w-[50vw] p-5 h-[50vh] xl:h-[100vh] flex items-center justify-center flex-col">
         <div className="text-4xl mx-5">New Company</div>
-        <div className="text-lg m-5">Add comany name to the poupup box.</div>
+        <div className="text-lg m-5">Add comany name to the popup box.</div>
         <Modal platformName={platformName} />
       </div>
     </div>
