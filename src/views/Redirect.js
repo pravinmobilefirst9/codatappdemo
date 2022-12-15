01be7d1ca1141db63fca8552c8e45aeb86a8df2f
Jaydeep Patel <58411819+jaydeep-patel-au6@users.noreply.github.com>
2022-12-15 18:48:28 +0530

    Update Redirect.js

--- src/views/Redirect.js
+++ src/views/Redirect.js
@@ -4,31 +4,47 @@ import { useNavigate } from "react-router-dom";
 import { baseURL } from "../data/const";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Oval } from "react-loader-spinner";
 
 const Redirect = () => {
+  const [state, setState] = useState(true);
+  const [link, setLink] = useState(false);
   const companyId = localStorage.getItem("companyId");
   const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      authApiCall();
+      if (link) {
+        toast.success("Authentication done", {
+          position: toast.POSITION.TOP_RIGHT,
+        });
+        setTimeout(() => {
+          navigate("/invoice");
+          setLink(false);
+        }, 1000);
+      }
+      setState(!state);
+    }, 5000);
+  }, [state]);
+
   const authApiCall = () => {
-    navigate("/invoice");
-    // axios
-    //   .get(baseURL + `api/InitializeConnection/${companyId}`)
-    //   .then((response) => {
-    //     if (Object.values(response?.data)[0].results[0]?.status === "Linked") {
-    //       navigate("/invoice");
-    //       toast.success("Authentication done", {
-    //         position: toast.POSITION.TOP_RIGHT,
-    //       });
-    //     } else {
-    //       toast.error("Some Error happning please try again in some time !", {
-    //         position: toast.POSITION.TOP_RIGHT,
-    //       });
-    //     }
-    //   })
-    //   .catch((err) => {
-    //     toast.error("Some Error happning please try again later !", {
-    //       position: toast.POSITION.TOP_RIGHT,
-    //     });
-    //   });
+    axios
+      .get(baseURL + `api/InitializeConnection/${companyId}`)
+      .then((response) => {
+        if (Object.values(response?.data)[0].results[0]?.status === "Linked") {
+          // navigate("/invoice");
+          setLink(true);
+        }
+      })
+      .catch((err) => {
+        toast.error(
+          "Some Error happning please try again later or refresh the page !",
+          {
+            position: toast.POSITION.TOP_RIGHT,
+          }
+        );
+      });
   };
 
   return (
@@ -37,16 +53,31 @@ const Redirect = () => {
       <div>
         <div className="relative w-[100vw] p-5 bg-slate-50 h-[100vh] flex items-center justify-center flex-col">
           <div className="text-4xl mx-5">Redirect to Invoice page</div>
-          <div className="text-lg m-5">
-            Click below button after authenticate
+          <div className="m-10">
+            <Oval
+              height={30}
+              width={30}
+              color="white"
+              wrapperStyle={{}}
+              wrapperclassName=""
+              visible={true}
+              ariaLabel="oval-loading"
+              secondaryColor="purple"
+              strokeWidth={2}
+              strokeWidthSecondary={2}
+            />
+          </div>
+          <div className="text-lg">
+            Please do not close this page. Wait for some time to redirect to
+            invoice page.
           </div>
           <div>
-            <button
+            {/* <button
               onClick={authApiCall}
               className={` active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple px-5 mt-5 py-3 font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg`}
-            >
-              <>Click Here to go Invoice page</>
-            </button>
+            > */}
+
+            {/* </button> */}
           </div>
           <div className="absolute top-10 left-10 text-4xl">CODAT.IO</div>
         </div>
