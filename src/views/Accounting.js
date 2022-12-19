2568537aefbf06b14bce16dbad3c7b02ed3199b8
Jaydeep Patel <58411819+jaydeep-patel-au6@users.noreply.github.com>
2022-12-19 18:57:58 +0530

    Update Accounting.js

--- src/views/Accounting.js
+++ src/views/Accounting.js
@@ -5,7 +5,7 @@ import SoftwareButton from "../components/Accounting/SoftwareButton";
 const Accounting = () => {
   return (
     <div className="bg-slate-50 lg:w-screen lg:h-screen p-5 md:p-10">
-      <div className="text-2xl xl:text-4xl font-bold">Accounting Details</div>
+      <div className="text-2xl xl:text-4xl font-bold">Account Reconciliation</div>
       <div>powered by CODAT</div>
       <div className="flex items-center justify-between">
         <div className="mt-32 md:ml-28 flex items-center justify-center max-w-[1000px] lg:justify-start">
